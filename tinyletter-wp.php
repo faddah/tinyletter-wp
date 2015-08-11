ed22db11fe9e20a45b8d186602c7c723454cb0b2
faddah <my_biz@me.com>
2015-08-10 18:33:36 -0700

    created basic php file structure & WordPress plugin comment requirements in head of tinyletter-wp.php file.

---
+++ tinyletter-wp.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package tinyletter-wp
+ * @version 0.0.1b
+ */
+/*
+Plugin Name: TinyLetter WordPress
+Plugin URI: http://clicknathan.com/web-design/tinyletter-wordpress-plugin/
+Description: A WordPress Plugin to sign-up users for the WordPress Site's TinyLetter.com subscription, as a small widget in a Post, Page, Sidebar or Footer.
+Version: 0.0.1b
+Author URI: http://clicknathan.com/ & https://github.com/faddah/
+License: GPL
+*/
+
+
+
+?>

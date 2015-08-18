362642b8a575a80a0f167ae8b5df7b5ae6ccd643
faddah <my_biz@me.com>
2015-08-17 22:22:14 -0700

    Added beginnings of skeletal framework to add plug-in to widget page section and register widget in PHP - most code in functions eempty... for now.

--- tinyletter-wp.php
+++ tinyletter-wp.php
@@ -12,6 +12,48 @@ Author URI: http://clicknathan.com/ & https://github.com/faddah/
 License: GPL
 */
 
+// actions
 
+add_action('widgets_init', 'tinyletter_wp_widget_init');
+
+/* *
+ * register widget
+ */
+
+function tinyletter_wp_widget_init() {
+  register_widget(TinyLetter_WP_Widget);
+}
+
+/* *
+ * widget class
+ */
+
+class TinyLetter_WP_Widget extends WP_Widget {
+  public function TinyLetter_WP_Widget() {
+    # code...
+  }
+
+  /* *
+  * show widget form in Appearance / Widgets
+  */
+  function form($instance) {
+
+  }
+
+  /* *
+  * save widget form
+  */
+  function update($new_instance, $old_instance) {
+
+  }
+
+  /* *
+  * show widget in Post / Page
+  */
+  function widget($args, $instance) {
+
+  }
+
+}
 
 ?>
